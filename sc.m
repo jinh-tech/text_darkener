263c54212119ec3f3359b9e91d1097977dd44138
Nitish Joshi <nitishjoshi25051994@gmail.com>
2015-01-28 15:20:20 +0530

    Samples deleted. Value of 'pixels' changed to 5 from 10

--- sc.m
+++ sc.m
@@ -1,28 +1,39 @@
 clear;
 clc;
+
 img = imread("sample2.jpg");
+
 h = waitbar(0,'0%');
+pixels=5;
+times=2;
 spic = size(img,1)*size(img,2);
-for v = [10]
+
+for v = [pixels]
+
 	x = size(img,1);
 	y = size(img,2);
 	x-=v;
 	y-=v;
 	img1 = img;
+
 	for i=v+1:x
+
  		for j=v+1:y
+
  			red=img(i-v:i+v,j-v:j+v,1)(:);
  			green=img(i-v:i+v,j-v:j+v,2)(:);
  			blue=img(i-v:i+v,j-v:j+v,3)(:);
  			temp=(mean(red-img(i,j,1))+mean(green-img(i,j,2))+mean(blue-img(i,j,3)))/3;
- 			img1(i,j,1)-=2*temp;
-			img1(i,j,2)-=2*temp;
-			img1(i,j,3)-=2*temp;
+ 			img1(i,j,:)-=times*temp;
+
 		end
+
 	completed = (i*y)/spic;
 	waitbar(completed,h,sprintf('%d%%',completed*100));	
+
  	end
+
 	close(h);
-	imwrite(img1,"sc_sample2.jpg");
+	imwrite(img1,"sample2.jpg");
 
 end
